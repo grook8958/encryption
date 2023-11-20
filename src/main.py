36e75ae1c4cacc42cdf6085c30b06192242a1b49
grook8958 <clement.puks@gmail.com>
2023-11-21 00:47:54 +0100

    refactor: Renamed tn_caesar_execute_callback to tn_execute_callback and added extra logic for the callback to work for every encryption

--- src/main.py
+++ src/main.py
@@ -19,20 +19,24 @@ with dpg.theme() as global_theme:
 dpg.bind_font(font=default_font)
 
 # Callback for encrypt/decrypt buttons
-def btn_execute_caesar_callback(sender, app_data, user_data):
-    # user_data = { input: text_input, output: text_output, action: encrypt/decrypt }
+def btn_execute_callback(sender, app_data, user_data):
+    # user_data = { input: text_input, output: text_output, action: encrypt/decrypt, encryption: caesar,etc... }
     input = user_data['input']
     output = user_data['output']
     action = user_data['action']
+    encryption = user_data['encryption']
     value = dpg.get_value(item=input)
-    if action == 'encrypt':
-        # Encrypt the entered text
-        output_text = Caesar.encrypt(value)
-    elif action == 'decrypt':
-        # Decrypt the entered text
-        output_text = Caesar.decrypt(value)
+    if encryption == 'caesar':
+        if action == 'encrypt':
+            # Encrypt the entered text
+            output_text = Caesar.encrypt(value)
+        elif action == 'decrypt':
+            # Decrypt the entered text
+            output_text = Caesar.decrypt(value)
     dpg.set_value(item=output, value=output_text)
-    print(f'Input: {input}\nOutput: {output}\nAction: {action}\nValue: {value}\nOuput Text: {output_text}\n-----------')
+    print(f'Caesar Encryption/Decryption\nInput: {input}\nOutput: {output}\nAction: {action}\nValue: {value}\nOuput Text: {output_text}\n-----------')
+
+    
 
 # Switch between encrypt and decrypt modes
 def btn_switch_callback(sender, app_data, user_data):
@@ -50,12 +54,7 @@ def btn_switch_callback(sender, app_data, user_data):
           
 
 with dpg.window(label="Chiffrement ROT13", width=200, pos=(500,300)):
-    # user data and callback set when button is created
-    dpg.add_button(label="Apply", callback=btn_execute_caesar_callback, user_data="Some Data")
-    # user data and callback set any time after button has been created
-    btn = dpg.add_button(label="Apply 2", )
-    dpg.set_item_callback(btn, btn_execute_caesar_callback)
-    dpg.set_item_user_data(btn, "Some Extra User Data")
+    dpg.add_text('Work in Progress')
 
 # Encrypt/Decrypt Menu with Caesar Encryption
 with dpg.window(label="Chiffrement de César", autosize=True):
@@ -65,26 +64,26 @@ with dpg.window(label="Chiffrement de César", autosize=True):
         dpg.bind_item_font(text, default_font_bold)
         encrypt_text_input = dpg.add_input_text(default_value='...', height=100, width=450, multiline=True, tag='encrypt_text_input')
         with dpg.group(horizontal=True):
-            btn_encrypt_caesar = dpg.add_button(label="Chiffrer", callback=btn_execute_caesar_callback)
+            btn_encrypt_caesar = dpg.add_button(label="Chiffrer", callback=btn_execute_callback)
             btn_switch_caesar = dpg.add_button(label='Inversé', callback=btn_switch_callback, user_data=('caesar','encrypt'))
         dpg.add_text('\n')
         text = dpg.add_text('Texte Chiffré')
         dpg.bind_item_font(text, default_font_bold)
         encrypt_text_output = dpg.add_input_text(default_value="...", height=100, width=450, multiline=True, enabled=False, tag='encrypt_text_output')
-        dpg.set_item_user_data(item=btn_encrypt_caesar, user_data={'input': encrypt_text_input, 'output': encrypt_text_output, 'action': 'encrypt'})
+        dpg.set_item_user_data(item=btn_encrypt_caesar, user_data={'input': encrypt_text_input, 'output': encrypt_text_output, 'action': 'encrypt', 'encryption': 'caesar'})
     # Decrypt Caesar
     with dpg.child_window(height=340,width=500, label='Déchiffrer', no_scrollbar=True, no_scroll_with_mouse=True, show=False, tag='window_decrypt_caesar'):
         text = dpg.add_text('Texte Chiffré')
         dpg.bind_item_font(text, default_font_bold)
         decrypt_text_input = dpg.add_input_text(default_value="...", height=100, width=450, multiline=True, tag='decrypt_text_input')
         with dpg.group(horizontal=True):
-            btn_decrypt_caesar = dpg.add_button(label="Déchiffer", user_data='text', callback=btn_execute_caesar_callback)
+            btn_decrypt_caesar = dpg.add_button(label="Déchiffer", user_data='text', callback=btn_execute_callback)
             btn_switch_caesar = dpg.add_button(label='Inversé', callback=btn_switch_callback, user_data=('caesar','decrypt'))
         dpg.add_text('\n')
         text = dpg.add_text('Texte au Clair')
         dpg.bind_item_font(text, default_font_bold)
         decrypt_text_output = dpg.add_input_text(default_value="...", height=100, width=450, multiline=True, enabled=False, tag='decrypt_text_output')
-        dpg.set_item_user_data(item=btn_decrypt_caesar, user_data={'input': decrypt_text_input, 'output': decrypt_text_output, 'action': 'decrypt'})
+        dpg.set_item_user_data(item=btn_decrypt_caesar, user_data={'input': decrypt_text_input, 'output': decrypt_text_output, 'action': 'decrypt', 'encryption': 'caesar'})
     # Set local theme
     with dpg.theme() as btn1_theme:
         with dpg.theme_component(dpg.mvAll):
