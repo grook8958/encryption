3fc89fc9e26e154b12459cf421d35a447da76ceb
grook8958 <clement.puks@gmail.com>
2023-11-24 13:32:46 +0100

    refactor: Added assets registry

---
+++ src/assets/__init__.py
@@ -0,0 +1,1 @@
+from assets.asset_registry import *
---
+++ src/assets/asset_registry.py
@@ -0,0 +1,9 @@
+import dearpygui.dearpygui as dpg
+
+with dpg.texture_registry(show=True):
+    width, height, channels, data = dpg.load_image('C:/Users/Eleve/cryptages/src/assets/copy_clipboard.png')
+    copy_clipboard = dpg.add_static_texture(width=width, height=height, default_value=data, tag="copy_clipboard")
+
+
+
+
