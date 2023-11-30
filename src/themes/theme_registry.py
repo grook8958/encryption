b8d6ecfee9d5554898ffd447a72c9f51a001b437
grook8958 <clement.puks@gmail.com>
2023-11-30 22:18:52 +0100

    feat: Added new themes inline with the the new language selecter

--- src/themes/theme_registry.py
+++ src/themes/theme_registry.py
@@ -9,3 +9,15 @@ with dpg.theme() as global_theme:
 with dpg.theme() as btn1_theme:
     with dpg.theme_component(dpg.mvAll):
         dpg.add_theme_color(dpg.mvThemeCol_Button, (0, 119, 200, 153), category=dpg.mvThemeCat_Core)
+
+# Register the No padding theme
+with dpg.theme() as nopad_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 4, 0)
+        dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 0, 0)
+
+
+# Register the Select Theme
+with dpg.theme() as sel_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 0, 4)
