17354b3e818777562ed061a661f17371caa58405
grook8958 <clement.puks@gmail.com>
2023-11-30 22:16:24 +0100

    chore: Removed useless lines
    fix: Texture registry showing up

--- src/assets/asset_registry.py
+++ src/assets/asset_registry.py
@@ -2,11 +2,7 @@ import dearpygui.dearpygui as dpg
 from utils import util
 
 # Register all textures assets
-with dpg.texture_registry(show=True):
+with dpg.texture_registry():
     copy_clipboard_path = util.resource_path('src/assets/copy_clipboard.png')
     width, height, channels, data = dpg.load_image(copy_clipboard_path)
     copy_clipboard = dpg.add_static_texture(width=width, height=height, default_value=data, tag="copy_clipboard")
-
-
-
-
