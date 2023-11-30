47b2f70530540f9ac63a9462c6fc0234894f1e92
grook8958 <clement.puks@gmail.com>
2023-11-30 22:18:01 +0100

    fix: Some UNICODE characters not showing up

--- src/themes/font_registry.py
+++ src/themes/font_registry.py
@@ -5,6 +5,8 @@ from utils import util
 with dpg.font_registry() as main_font_registry:
     default_font_path = util.resource_path('src/assets/Open_Sans/static/OpenSans-Regular.ttf')
     default_font_bold_path = util.resource_path('src/assets/Open_Sans/static/OpenSans-Bold.ttf')
-    default_font = dpg.add_font(default_font_path, size=18)
+    with dpg.font(default_font_path, size=18) as default_font:
+        dpg.add_font_range(0x0001, 0xffff)
     default_font_bold = dpg.add_font(default_font_bold_path, size=18)
     header1_font = dpg.add_font(default_font_bold_path, size=32)
+    
